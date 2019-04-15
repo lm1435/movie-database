b927dc7d43db0cf1f3e24a054d8dc29dee978b88
Luis Meza <luismeza01@gmail.com>
2019-04-14 19:58:00 -0500

    added the actual components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    movies: [],
+    error: null
+  }
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9725571b96179202ebd3830a5ee14d01&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch (e) {
+      console.log(e);
+      this.setState({
+        error: true
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {'<Luis\' Favorite Movies />'}
         </header>
+        {!this.state.error ?
+          <React.Fragment>
+            {this.state.movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
+          </React.Fragment>
+          : <p>An Error Occured Please Try Again.</p>
+          
+        }
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string,
+      overview: PropTypes.string
+    }),
+  }
+
+  
+  render() {
+    const { title, overview } = this.props.movie;
+    return (
+      <div>
+        <h3>{title}</h3>
+        <p>{overview}</p>
+      </div>
+    )
+  }
+  
+}
