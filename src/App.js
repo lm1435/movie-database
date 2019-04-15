e47962c74429800af201a13318df48981bfc8ab9
Luis Meza <luismeza01@gmail.com>
2019-04-14 20:18:06 -0500

    adding airbnb linter

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@ import Movie from './Movie';
 class App extends Component {
   state = {
     movies: [],
-    error: null
+    error: null,
   }
+
   async componentDidMount() {
     try {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9725571b96179202ebd3830a5ee14d01&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch (e) {
-      console.log(e);
+      console.log(e); // eslint-disable-line no-console
       this.setState({
-        error: true
-      })
+        error: true,
+      });
     }
   }
 
   render() {
+    const { error, movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           {'<Luis\' Favorite Movies />'}
         </header>
-        {!this.state.error ?
-          <React.Fragment>
-            {this.state.movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
-          </React.Fragment>
+        {!error
+          ? (
+            <React.Fragment>
+              {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+            </React.Fragment>
+          )
           : <p>An Error Occured Please Try Again.</p>
-          
         }
       </div>
     );
