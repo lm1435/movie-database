ba5d87a50b02128c096de27b3c103d6381050a13
Luis Meza <luismeza01@gmail.com>
2019-04-16 13:58:54 -0500

    adding path for movie posters'

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
+
 const Movie = ({
   movie: {
     title,
     overview,
+    poster_path: posterPath,
   },
 }) => (
   <div>
+    <img src={`${POSTER_PATH}${posterPath}`} alt={title} />
     <h3>{title}</h3>
     <p>{overview}</p>
   </div>
@@ -16,6 +20,7 @@ const Movie = ({
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    overview: PropTypes.string,
   }).isRequired,
 };
 
