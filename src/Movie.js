61feb7c1747f7ded20a4ebfbda078478b82f6f8e
Luis Meza <luismeza01@gmail.com>
2019-04-16 13:26:00 -0500

    adding propTypes for the FSC

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Movie = (props) => {
-  const {
-    movie: {
-      title,
-      overview,
-    },
-  } = props;
-  return (
-    <div>
-      <h3>{title}</h3>
-      <p>{overview}</p>
-    </div>
-  );
+const Movie = ({
+  movie: {
+    title,
+    overview,
+  },
+}) => (
+  <div>
+    <h3>{title}</h3>
+    <p>{overview}</p>
+  </div>
+);
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Movie;
