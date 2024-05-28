ef983baa61751b1d8493bdb89f796e0241a93c1c
Luis Meza <luismeza01@gmail.com>
2024-05-28 12:34:34 -0600

    commenting about the api key

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import PropTypes from 'prop-types';
-import MoviesList from './MoviesList';
-import MovieDetail from './MovieDetail';
-import rootReducer from './rootReducer';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import PropTypes from "prop-types";
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
+import rootReducer from "./rootReducer";
 
-const store = createStore(
-  rootReducer,
-  {},
-  composeWithDevTools(),
-);
+const store = createStore(rootReducer, {}, composeWithDevTools());
 const PAGE_PARAM = 1;
-const URL = 'https://api.themoviedb.org/3/discover/movie?api_key=9725571b96179202ebd3830a5ee14d01&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=';
+// this is just for a sample project. ideally this would not be in the FE since anything is exposed if its on the client.
+const URL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=9725571b96179202ebd3830a5ee14d01&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=";
 
 export default class App extends Component {
   state = {
@@ -28,7 +21,7 @@ export default class App extends Component {
     error: null,
     isLoading: true,
     pageNumber: PAGE_PARAM,
-  }
+  };
 
   componentDidMount() {
     const { pageNumber } = this.state;
@@ -38,47 +31,55 @@ export default class App extends Component {
   fetchMoreMovies = (page) => {
     const { movies } = this.state;
     fetch(`${URL}${page}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((res) => {
         this.setState({
-          movies: [
-            ...movies,
-            ...res.results,
-          ],
+          movies: [...movies, ...res.results],
           isLoading: false,
         });
       })
-      .catch(error => this.setState({ error, isLoading: false }));
-  }
+      .catch((error) => this.setState({ error, isLoading: false }));
+  };
 
   updatePageNumber = (pageNumber) => {
     this.setState({ pageNumber });
     this.fetchMoreMovies(pageNumber);
-  }
+  };
 
   render() {
     const { error, isLoading } = this.state;
     return (
       <Provider store={store}>
-        {error ? 'OOPS, please refresh your page' : (
+        {error ? (
+          "OOPS, please refresh your page"
+        ) : (
           <React.Fragment>
-            {isLoading ? 'elavator music'
-              : (
-                <Router>
-                  <div className="App">
-                    <header className="App-header">
-                      <Link to="/">
-                        {'<Luis\' Movies Database/>'}
-                      </Link>
-                    </header>
-                    <Switch>
-                      <Route exact path="/" render={props => (<MoviesList {...props} state={this.state} fetchMoreMovies={this.fetchMoreMovies} updatePageNumber={this.updatePageNumber} />)} />
-                      <Route exact path="/:id" component={MovieDetail} />
-                    </Switch>
-                  </div>
-                </Router>
-              )
-            }
+            {isLoading ? (
+              "elavator music"
+            ) : (
+              <Router>
+                <div className="App">
+                  <header className="App-header">
+                    <Link to="/">{"<Luis' Movies Database/>"}</Link>
+                  </header>
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={(props) => (
+                        <MoviesList
+                          {...props}
+                          state={this.state}
+                          fetchMoreMovies={this.fetchMoreMovies}
+                          updatePageNumber={this.updatePageNumber}
+                        />
+                      )}
+                    />
+                    <Route exact path="/:id" component={MovieDetail} />
+                  </Switch>
+                </div>
+              </Router>
+            )}
           </React.Fragment>
         )}
       </Provider>
